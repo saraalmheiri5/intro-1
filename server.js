72c6df685a8ff7a11fb87e3bed66091def624915
saraalmheiri5 <g00092087@aus.edu>
2025-02-18 12:59:47 +0400

    changes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 8080;
+
+// Basic route to check server
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
